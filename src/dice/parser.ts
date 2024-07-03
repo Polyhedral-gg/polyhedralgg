b72696b6dc8d8c3d3a2dc78f5ff2f637b1bc2c9e
ThisIsBrady <bradyh2001@gmail.com>
2024-07-02 23:36:02 -0500

    Respond to roll command

--- src/dice/parser.ts
+++ src/dice/parser.ts
@@ -1,5 +1,28 @@
 import * as ohm from "ohm-js";
 
+function getRand(min: number, max: number): number {
+    return Math.floor(Math.random() * max - min + 1) + min;
+}
+
+function rollDice(numDice: number, numSides: number): number[] {
+    const results: number[] = new Array<number>(numDice).fill(0);
+
+    return results.map(() => {
+        return getRand(1, numSides);
+    });
+}
+
+function processBasicDiceNotation(numDice: number, numSides: number): DiceParseResult {
+    const results = rollDice(numDice, numSides);
+
+    return {
+        val: results.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+        }, 0),
+        msg: `${numDice}d${numSides}(${results.join(", ")})`,
+    };
+}
+
 const diceGrammar = ohm.grammar(String.raw`
     DiceNotation {
         Exp
@@ -30,7 +53,7 @@ const diceGrammar = ohm.grammar(String.raw`
     }
 `);
 
-interface Res {
+export interface DiceParseResult {
     val: number;
     msg: string;
 }
@@ -38,56 +61,67 @@ interface Res {
 const sem = diceGrammar.createSemantics();
 
 // Many cursed type assertions incoming because Ohm's api is JS and ESLint doesn't like that
-sem.addOperation<Res>("eval", {
-    number(_): Res {
+sem.addOperation<DiceParseResult>("eval", {
+    number(_): DiceParseResult {
         return { val: parseInt(this.sourceString), msg: this.sourceString };
     },
-    diceResult(a: unknown, _, b: unknown): Res {
-        const numDice = (a as { eval(): Res }).eval();
-        const numSides = (b as { eval(): Res }).eval();
-        return { val: 1, msg: `${numDice.msg}d${numSides.msg}` };
+    diceResult(a: unknown, _, b: unknown): DiceParseResult {
+        const numDice = (a as { eval(): DiceParseResult }).eval();
+        const numSides = (b as { eval(): DiceParseResult }).eval();
+        return processBasicDiceNotation(numDice.val, numSides.val);
     },
-    AddExp_plus(a: unknown, _, b: unknown): Res {
-        const left = (a as { eval(): Res }).eval();
-        const right = (b as { eval(): Res }).eval();
-        return { val: left.val + right.val, msg: `${left.msg}+${right.msg}` };
+    AddExp_plus(a: unknown, _, b: unknown): DiceParseResult {
+        const left = (a as { eval(): DiceParseResult }).eval();
+        const right = (b as { eval(): DiceParseResult }).eval();
+        return { val: left.val + right.val, msg: `${left.msg} + ${right.msg}` };
     },
-    AddExp_minus(a: unknown, _, b: unknown): Res {
-        const left = (a as { eval(): Res }).eval();
-        const right = (b as { eval(): Res }).eval();
-        return { val: left.val - right.val, msg: `${left.msg}-${right.msg}` };
+    AddExp_minus(a: unknown, _, b: unknown): DiceParseResult {
+        const left = (a as { eval(): DiceParseResult }).eval();
+        const right = (b as { eval(): DiceParseResult }).eval();
+        return { val: left.val - right.val, msg: `${left.msg} - ${right.msg}` };
     },
-    MulExp_times(a: unknown, _, b: unknown): Res {
-        const left = (a as { eval(): Res }).eval();
-        const right = (b as { eval(): Res }).eval();
-        return { val: left.val * right.val, msg: `${left.msg}*${right.msg}` };
+    MulExp_times(a: unknown, _, b: unknown): DiceParseResult {
+        const left = (a as { eval(): DiceParseResult }).eval();
+        const right = (b as { eval(): DiceParseResult }).eval();
+        return { val: left.val * right.val, msg: `${left.msg} * ${right.msg}` };
     },
-    MulExp_divide(a: unknown, _, b: unknown): Res {
-        const dividend = (a as { eval(): Res }).eval();
-        const divisor = (b as { eval(): Res }).eval();
-        return { val: dividend.val / divisor.val, msg: `${dividend.msg}÷${divisor.msg}` };
+    MulExp_divide(a: unknown, _, b: unknown): DiceParseResult {
+        const dividend = (a as { eval(): DiceParseResult }).eval();
+        const divisor = (b as { eval(): DiceParseResult }).eval();
+        return { val: dividend.val / divisor.val, msg: `${dividend.msg} ÷ ${divisor.msg}` };
     },
-    ExpExp_power(a: unknown, _, b: unknown): Res {
-        const base = (a as { eval(): Res }).eval();
-        const exp = (b as { eval(): Res }).eval();
-        return { val: base.val ** exp.val, msg: `${base.msg}^${exp.msg}` };
+    ExpExp_power(a: unknown, _, b: unknown): DiceParseResult {
+        const base = (a as { eval(): DiceParseResult }).eval();
+        const exp = (b as { eval(): DiceParseResult }).eval();
+        return { val: base.val ** exp.val, msg: `${base.msg} ^ ${exp.msg}` };
     },
-    PriExp_paren(_, a: unknown, __): Res {
-        const res = (a as { eval(): Res }).eval();
+    PriExp_paren(_, a: unknown, __): DiceParseResult {
+        const res = (a as { eval(): DiceParseResult }).eval();
         return { val: res.val, msg: `(${res.msg})` };
     },
-    PriExp_pos(_, a: unknown): Res {
-        const res = (a as { eval(): Res }).eval();
+    PriExp_pos(_, a: unknown): DiceParseResult {
+        const res = (a as { eval(): DiceParseResult }).eval();
         return { val: Math.abs(res.val), msg: `abs(${res.msg})` };
     },
-    PriExp_neg(_, a: unknown): Res {
-        const res = (a as { eval(): Res }).eval();
+    PriExp_neg(_, a: unknown): DiceParseResult {
+        const res = (a as { eval(): DiceParseResult }).eval();
         return { val: -res.val, msg: `-(${res.msg})` };
     },
 });
 
-const matchResult = diceGrammar.match("1 + 2 + ((4d3 / 3d1) ^ 3) - 4 * -3 / 2");
-const adapter = sem(matchResult);
-const res: Res = (adapter.eval as () => Res)();
-console.log(res.msg);
-console.log(res.val);
+// const matchResult = diceGrammar.match("1 + 2 + ((4d3 / 3d1) ^ 3) - 4 * -3 / 2");
+// const adapter = sem(matchResult);
+// const res: DiceParseResult = (adapter.eval as () => DiceParseResult)();
+// console.log(res.msg);
+// console.log(res.val);
+
+export default function parse(notation: string): DiceParseResult {
+    const matchResult = diceGrammar.match(notation);
+
+    if (matchResult.failed()) {
+        throw Error("Failed to parse dice notation");
+    }
+
+    const adapter = sem(matchResult);
+    return (adapter.eval as () => DiceParseResult)();
+}
--- src/discord/commands/roll.ts
+++ src/discord/commands/roll.ts
@@ -1,11 +1,41 @@
-import { InteractionResponseType, type APIChatInputApplicationCommandInteraction } from "discord-api-types/v10";
+import {
+    type APIApplicationCommandInteractionDataStringOption,
+    InteractionResponseType,
+    MessageFlags,
+    type APIChatInputApplicationCommandInteraction,
+} from "discord-api-types/v10";
 import { NextResponse } from "next/server";
+import parse, { type DiceParseResult } from "~/dice/parser";
 
 export default function roll(interaction: APIChatInputApplicationCommandInteraction): NextResponse {
+    if (!interaction.data.options || interaction.data.options.length < 1) {
+        return NextResponse.json({
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                content: "Please enter the notation to run!",
+                flags: MessageFlags.Ephemeral,
+            },
+        });
+    }
+
+    let msg = "";
+    try {
+        const notation = interaction.data.options[0] as APIApplicationCommandInteractionDataStringOption;
+        const DiceRes: DiceParseResult = parse(notation.value);
+
+        msg = `${DiceRes.msg} = **${DiceRes.val}**`;
+    } catch (error) {
+        if (error instanceof Error) {
+            msg = error.message;
+        } else {
+            msg = "An error occured while parsing the command";
+        }
+    }
+
     return NextResponse.json({
         type: InteractionResponseType.ChannelMessageWithSource,
         data: {
-            content: "Result: 2",
+            content: msg,
         },
     });
 }
